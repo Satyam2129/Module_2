7742f20f0195ca9644e654d783f0cb60615a06f0
Satyam2129 <satyam.singh_cs20@gla.ac.in>
2021-11-13 00:36:06 +0530

    Initial commit

---
+++ Assignment_1.java
@@ -0,0 +1,34 @@
+public class Assignment_1 {
+    public static void main(String[] args) {
+        System.out.println("Integer Range: ");
+        Integer i1 = Integer.MIN_VALUE;
+        System.out.println("min = "+ i1);
+        Integer i2 = Integer.MAX_VALUE;
+        System.out.println("max = "+ i2);
+        System.out.println("Double Range: ");
+        Double i3 = Double.MIN_VALUE;
+        System.out.println("min = "+ i3);
+        Double i4 = Double.MAX_VALUE;
+        System.out.println("max = "+ i4);
+        System.out.println("Long Range: ");
+        Long i5 = Long.MIN_VALUE;
+        System.out.println("min = "+ i5);
+        Long i6 = Long.MAX_VALUE;
+        System.out.println("max = "+ i6);
+        System.out.println("Short Range: ");
+        Short i7= Short.MIN_VALUE;
+        System.out.println("min = "+ i7);
+        Short i8= Short.MAX_VALUE;
+        System.out.println("max = "+ i8);
+        System.out.println("Character Range: ");
+        Character i9 = Character.MIN_VALUE;
+        System.out.println("min = "+ i9);
+        Character i10 = Character.MAX_VALUE;
+        System.out.println("max = "+ i10);
+        System.out.println("Byte Range: ");
+        Byte obj = Byte.MIN_VALUE;
+        System.out.println("min = "+obj);
+        Byte o1= Byte.MAX_VALUE;
+        System.out.println("max = "+o1);
+    }
+}
